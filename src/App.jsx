d1283d51fe743d2e2eeca7641c054398263259ba
sharmin133 <mstsharmin1781@gmail.com>
2025-06-08 22:18:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1 className='text-5xl'>welcome to wholesale platform</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import Root from './Layout/Root';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Root></Root>,
+  },
+]);
+
+
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
